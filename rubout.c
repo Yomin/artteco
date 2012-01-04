1952be51d6aecce727b70537b788b9b8fce1dbfc
Martin Roedel <mr@travelping.com>
2012-01-04 08:36:50 +0100

    rubout logging added

--- rubout.c
+++ rubout.c
@@ -54,23 +54,34 @@ void rubout()
     }
 }
 
-void rubout_register_s(rubout_func* f)
+DEBUGIZE_1(void, rubout_register_s, rubout_func* f)
 {
-    stack_push_s(&f, &stk_rubout);
+    DEBUG(
+        stack_push_s(&f, &stk_rubout),
+        stack_push_s_dbg(&f, &stk_rubout, dbg_arg, dbg_func)
+    );
     stack_exec(&stk_counter);
     char counter;
     stack_top(&counter, &stk_counter);
 }
 
-void rubout_register(rubout_func* f, void* data, int size)
+DEBUGIZE_3(void, rubout_register, rubout_func* f, void* data, int size)
 {
+#ifdef NDEBUG
     rubout_save(data, size);
     rubout_register_s(f);
+#else
+    rubout_save_dbg(data, size, dbg_arg2, dbg_arg3, dbg_func);
+    rubout_register_s_dbg(f, dbg_arg1, dbg_func);
+#endif
 }
 
-void rubout_save(void* data, int size)
+DEBUGIZE_2(void, rubout_save, void* data, int size)
 {
-    stack_push(data, size, &stk_rubout);
+    DEBUG(
+        stack_push(data, size, &stk_rubout),
+        stack_push_dbg(data, size, &stk_rubout, dbg_arg1, dbg_arg2, dbg_func)
+    );
 }
 
 int rubout_load(void* data)
--- rubout.h
+++ rubout.h
@@ -7,13 +7,15 @@
 #ifndef __ART_TECO_RUBOUT__
 #define __ART_TECO_RUBOUT__
 
+#include "debug.h"
+
 typedef void rubout_func();
 
 void rubout_init();
 void rubout_finish();
-void rubout_register_s(rubout_func* f);
-void rubout_register(rubout_func* f, void* data, int size);
-void rubout_save(void* data, int size);
+DEBUGIZE_1(void, rubout_register_s, rubout_func* f);
+DEBUGIZE_3(void, rubout_register, rubout_func* f, void* data, int size);
+DEBUGIZE_2(void, rubout_save, void* data, int size);
 int  rubout_load(void* data);
 int  rubout_info();
 void rubout_start();
@@ -21,5 +23,11 @@ void rubout_end();
 void rubout_break();
 void rubout_clear();
 void rubout();
+
+#ifndef NDEBUG
+    #define rubout_register_s(f) rubout_register_s_dbg(f, #f, __func__)
+    #define rubout_register(f, data, size) rubout_register_dbg(f, data, size, #f, #data, #size, __func__)
+    #define rubout_save(data, size) rubout_save_dbg(data, size, #data, #size, __func__)
+#endif
 
 #endif
